2af45ecf66cd4e5924695fad4f9d3aef572f4b8a
Saurav Singh Mehta <sauravmehta631@gmail.com>
2026-01-03 18:21:20 +0530

    Add files via upload

---
+++ Frontend/src/assets/assests.js
@@ -0,0 +1,19 @@
+import lightBg from "./lightBg.mp4";
+import bgVideo from "./bgVideo.mp4";
+import blue from './blue.mp4';
+import water from './water.mp4';
+import ghostsmoke from './ghostsmoke.mp4';
+import cat from './cat.mp4';
+import fallback from './fallback.png'
+
+const Backgrounds = {
+    lightBg :lightBg,
+    bgVideo : bgVideo,
+    blue : blue,
+    water:water,
+    ghostsmoke:ghostsmoke,
+    cat:cat,
+    fallback:fallback
+}
+
+export { Backgrounds };
