5a65b827b4d2a999691ec37983b7d45427528b1b
Saurav s Mehta <sauravmehta631@gmail.com>
2026-05-31 19:10:18 +0530

    videos assets

---
+++ Frontend/src/assets/assests.js
@@ -0,0 +1,14 @@
+// src/assets/assests.js
+
+import video1 from "./video1.mp4";
+import video2 from "./video2.mp4";
+import video3 from "./video3.mp4";
+import video4 from "./video4.mp4";
+// import video5 from "./video5.mp4";
+
+export const Backgrounds = {
+  video1,
+  video2,
+  video3,
+  video4
+};
